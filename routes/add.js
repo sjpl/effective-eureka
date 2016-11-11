489b6013764f5c2b010c0d4a72c336e3b3c09953
sjpl <slaub@ucsd.edu>
2016-11-10 23:44:33 -0800

    Added a routes folder. Not currently in use.

---
+++ routes/add.js
@@ -0,0 +1,19 @@
+var data = require("data/data.json");
+
+exports.addEvent = function(req, res) {
+	// Your code goes here
+  console.log("yay, addEvent just ran!");
+    console.log(data.events);
+//    console.log(req.query.name);
+//    console.log(req.query.description);
+//    
+//    var newFriend = 
+//        {
+//            name: req.query.name,
+//            description: req.query.description,
+//            imageURL: "http://lorempixel.com/400/400/people"
+//        };
+//    console.log(newFriend);
+//    
+//    data.friends.push(newFriend);
+}
