798ca9f70fa4b3ba12b581a34d5481f337d7b953
sjpl <slaub@ucsd.edu>
2016-11-15 22:08:45 -0800

    Changed page names. Updated navbar and linking.

--- routes/eventlistpage.js
+++ routes/eventlistpage.js
@@ -3,5 +3,5 @@ var data = require('../data.json');
 
 //Load page
 exports.view = function(req, res){
-  res.render('hostlist',data);
+  res.render('eventlistpage',data);
 };
--- routes/hostlistpage.js
+++ routes/hostlistpage.js
@@ -3,5 +3,5 @@ var data = require('../data.json');
 
 //Load page
 exports.view = function(req, res){
-  res.render('indexin',data);
+  res.render('hostlistpage',data);
 };
